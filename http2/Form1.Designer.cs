fe3c56f70276fc8d1cbd7891d00ca86bb7131b1d
Fireplace <37326218+Edistein@users.noreply.github.com>
2018-12-25 18:29:58 +0000

    Add files via upload

---
+++ http2/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace http2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(11, 20);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(383, 85);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "运行";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button1_MouseMove);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(763, 27);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(134, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "100";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(424, 72);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(473, 37);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(672, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 21);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "重复次数";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox2.Location = new System.Drawing.Point(11, 264);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(909, 393);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Location = new System.Drawing.Point(424, 25);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 35);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "停止";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(7, 131);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 21);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "User";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(7, 194);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 21);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Password";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(104, 131);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(289, 43);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "/";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(104, 191);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(289, 43);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "/";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.Location = new System.Drawing.Point(13, 243);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "返回值";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(420, 134);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(74, 21);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "已完成：";
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("微软雅黑", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label8.Location = new System.Drawing.Point(500, 121);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(321, 53);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "0%";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 681);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "HttpBot";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ http2/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace http2
+{
+   
+public partial class Form1 : Form
+    {
+        bool stop = false;
+        public Form1()
+        {
+            
+            InitializeComponent();
+            
+        }
+        static int GetRandomSeed()
+        {
+            byte[] bytes = new byte[4];
+            System.Security.Cryptography.RNGCryptoServiceProvider rng = new System.Security.Cryptography.RNGCryptoServiceProvider();
+            rng.GetBytes(bytes);
+            return BitConverter.ToInt32(bytes, 0);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            stop = false;
+            textBox1.Enabled = false;
+            button1.Enabled = false;
+            Random ro = new Random(GetRandomSeed());
+            
+            string p =" 33550336";
+            progressBar1.Value = 0;
+            progressBar1.Maximum= Convert.ToInt32(textBox1.Text);
+            string[] str1 = { "b","p", "m" , "f" , "d","t" , "n" , "l" , "g" , "k" , "h" , "j" , "q" , "x" };
+            string[] str2 = { "1314","123","abc","abcabc","123123","666","654","111","000","aaa","1234","abcabc"};
+            string[] str3 = { "00","01","02","03","04","05","06","07"};
+            
+            for (int i = 1; i <= Convert.ToInt32(textBox1.Text); i++)
+            {
+                if (stop == true)
+                {
+                    progressBar1.Value = 0;
+                    label8.Text = "0%";
+                    break;
+                 }
+                string u = Convert.ToString(ro.Next(222,999))+ Convert.ToString(ro.Next(10, 99)) + Convert.ToString(ro.Next(10, 99)) + Convert.ToString(ro.Next(10, 999));
+                int mode =ro.Next(0,8);
+                if (mode == 0)
+                {
+                    int p1 = ro.Next(10000,999999);
+                    p = Convert.ToString(p1) + str1[ro.Next(0, 13)]+ str1[ro.Next(0, 13)]+str1[ro.Next(0, 13)];
+                }
+                if (mode == 1)
+                {
+                    int p1 = ro.Next(10000, 9999999);
+                   p = Convert.ToString(p1) + str2[ro.Next(0,11)];
+                }
+                if (mode ==2)
+                {
+                    int p1 = ro.Next(1, 13);
+                    int p2=ro.Next(1,31);
+                    p="20"+str3[ro.Next(0,7)]+Convert.ToString(p1)+Convert.ToString(p2)+str1[ro.Next(0, 13)] + str1[ro.Next(0, 13)] + str1[ro.Next(0, 13)];
+                }
+                if (mode == 3)
+                {
+                    int p1 = ro.Next(1, 13);
+                    int p2 = ro.Next(1, 31);
+                    p="19"+Convert.ToString(ro.Next(71,100))+ Convert.ToString(p1) + Convert.ToString(p2) + str1[ro.Next(0, 13)] + str1[ro.Next(0, 13)] + str1[ro.Next(0, 13)];
+                }
+                if (mode == 4)
+                {
+                    int p1 = ro.Next(10000, 999999);
+                   p = str1[ro.Next(0, 13)] + str1[ro.Next(0, 13)] + str1[ro.Next(0, 13)]+Convert.ToString(p1) ;
+                }
+                if (mode == 5)
+                {
+                    int p1 = ro.Next(10000, 9999999);
+                    p = str2[ro.Next(0, 11)]+Convert.ToString(p1)  ;
+                }
+                if (mode == 6)
+                {
+                    int p1 = ro.Next(1, 13);
+                    int p2 = ro.Next(1, 31);
+                    p = str1[ro.Next(0, 13)] + str1[ro.Next(0, 13)] + str1[ro.Next(0, 13)]+ "20" + str3[ro.Next(0, 7)] + Convert.ToString(p1) + Convert.ToString(p2) ;
+                }
+                if (mode == 7)
+                {
+                    int p1 = ro.Next(1, 13);
+                    int p2 = ro.Next(1, 31);
+                    p = str1[ro.Next(0, 13)] + str1[ro.Next(0, 13)] + str1[ro.Next(0, 13)]+ "19" + Convert.ToString(ro.Next(71, 100)) + Convert.ToString(p1) + Convert.ToString(p2) ;
+                }
+
+
+                try
+                {
+                    label4.Text = Convert.ToString(u);
+                    label5.Text = p;
+                    string post = "u=" + u+ "&p=" + p;
+                    textBox2.Text= "NO." + Convert.ToString(i) + ":   \r\n" + http.HttpPost("http://z1.ssd322.cn.com/", post);
+                   
+
+                }
+                catch (System.Exception e1)
+                {
+                    textBox2.Text ="NO."+Convert.ToString(i)+":   "+e1.Message + Environment.NewLine+textBox2.Text;
+                }
+                progressBar1.Value++;
+                label8.Text = Convert.ToString(Convert .ToSingle(i)/Convert.ToSingle(textBox1.Text)*100+"%");
+                Application.DoEvents();
+             }
+            textBox1.Enabled = true;
+            button1.Enabled = true;
+       
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            stop = true;
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            button1.BackColor = Color.Cyan;
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.WhiteSmoke;
+        }
+    }
+    public class http
+    {
+       public static string HttpPost(string Url, string postDataStr)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
+            request.Method = "POST";
+            request.KeepAlive = true;
+            request.ContentLength =postDataStr.Length ;
+            request.Accept = "text/html, application/xhtml+xml,application/xml;1=0.9,image/webq,image/apng,image/tpg,*/*;q=0.8";
+            request.UserAgent = "Mozilla/5.0 (Linux; Android 8.0.0; LON-AL00 Build/HUAWEILON-AL00; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/70.0.3538.80 Mobile Safari/537.36 V1_AND_SQ_7.7.6_898_GM_D PA QQ/7.7.6.3680 NetType/WIFI WebP/0.4.1 Pixel/1440";
+            request.ContentType = "application/x-www-form-urlencoded";
+           
+            request.Referer = "http://z1.ssd322.cn.com/cn2/login/h5_aq_login.asp";
+            request.CookieContainer = new CookieContainer();
+
+            request.ContentLength = Encoding.UTF8.GetByteCount(postDataStr);
+            // request.CookieContainer = Cookie;
+            Stream myRequestStream = request.GetRequestStream();
+            StreamWriter myStreamWriter = new StreamWriter(myRequestStream, Encoding.GetEncoding("gb2312"));
+            myStreamWriter.Write(postDataStr);
+            myStreamWriter.Close();
+  
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            // response.Cookies = cookie.GetCookies(response.ResponseUri);
+            Stream myResponseStream = response.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
+            string retString = myStreamReader.ReadToEnd();
+            myStreamReader.Close();
+            myResponseStream.Close();
+
+            return retString;
+        }
+    }
+}
